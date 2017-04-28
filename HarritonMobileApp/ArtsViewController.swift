cb2a692e0a9452c68e9a600a1b6c5f346996fc7a
Wil <wil444@live.com>
2017-04-27 21:52:09 -0400

    Cleaned up segmentControl and added text from website.

--- HarritonMobileApp/ArtsViewController.swift
+++ HarritonMobileApp/ArtsViewController.swift
@@ -49,17 +49,18 @@ class ArtsViewController: UIViewController {
         if segmentControl.selectedSegmentIndex == 0
         {
 
-            Label.text = "Test 1"
+            
+            Label.text = "\("The study of art provides students with new ways of seeing themselves and the world. In the supportive, respectful environment of our art classes, students are encouraged to express their ideas and come to understand the visions of their classmates and of professional artists. They are taught the language of aesthetics and how to think critically about the art that surrounds them. They also learn and practice the process of creation and engage in the many decisions that lead to a work of art.")\("\n\nA team of experienced and dedicated art teachers deliver a rich and varied art curriculum. Students acquire technical skills, a broad knowledge of materials and media and the discipline and dedication necessary to complete complex projects. Students have the opportunity to explore their interests and continue their studies by taking a sequenced series of art courses in the media that appeal to them.")\("\n\nAvailable courses encompass studio art, art history, art and modern culture, metal arts, ceramics, photography, film and visual arts. Levels range from introductory to AP.")"
         }
         
         if segmentControl.selectedSegmentIndex == 1
         {
-            Label.text = "Test 2"
+            Label.text = "Harriton is pleased to offer a variety of course offerings, including both performance and non-performance classes. Students at Harriton participate in a number of ensembles including Chorus, Concert Band, Concert Choir, Jazz Band, and Orchestra. In addition to the large ensembles, students also have the opportunity to perform in smaller chamber groups including Brass Ensemble, Chorale, String Quartet, and Woodwind Quintet. Our ensembles have received excellent and superior ratings at adjudicated festivals. In addition, our students are regularly chosen for participation in PMEA District, Region, and All-State Festivals."
         }
         
         if segmentControl.selectedSegmentIndex == 2
         {
-            Label.text = "Test 3"
+            Label.text = "\("The Harriton Theatre Company produces two shows a year, usually a drama or comedy in the Fall and a musical in the Spring. The company's purpose is to foster stage craft, and all members are encouraged to take part in all aspects of production – from performance to set crew. The Company Band consists of student musicians who perform between drama/comedy acts and provide the music for our musicals.")\("\n\nCurricular opportunities include creative expression and performance, with concentrations in acting, production and theater arts (drama survey and appreciation).")"
         }
         
         
