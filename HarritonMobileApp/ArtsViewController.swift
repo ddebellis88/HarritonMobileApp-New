5bef36e84c2a2a4c5163f3b4f1921861973aa242
David DeBellis <ddebellis88@aol.com>
2017-05-26 17:54:59 -0400

    whata app

--- HarritonMobileApp/ArtsViewController.swift
+++ HarritonMobileApp/ArtsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ArtsViewController: UIViewController {
-
+    
     @IBOutlet weak var Label: UITextView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var menuButton: UIBarButtonItem!
@@ -47,18 +47,18 @@ class ArtsViewController: UIViewController {
     @IBAction func ChangeLabel(_ sender: Any) {
         
         if segmentControl.selectedSegmentIndex == 0
-        {            
+        {
             Label.text = "\("The study of art provides students with new ways of seeing themselves and the world. In the supportive, respectful environment of our art classes, students are encouraged to express their ideas and come to understand the visions of their classmates and of professional artists. They are taught the language of aesthetics and how to think critically about the art that surrounds them. They also learn and practice the process of creation and engage in the many decisions that lead to a work of art.")\("\n\nA team of experienced and dedicated art teachers deliver a rich and varied art curriculum. Students acquire technical skills, a broad knowledge of materials and media and the discipline and dedication necessary to complete complex projects. Students have the opportunity to explore their interests and continue their studies by taking a sequenced series of art courses in the media that appeal to them.")\("\n\nAvailable courses encompass studio art, art history, art and modern culture, metal arts, ceramics, photography, film and visual arts. Levels range from introductory to AP.")"
         }
-        
             
-        
+            
+            
         else if segmentControl.selectedSegmentIndex == 1
         {
             Label.text = "Harriton is pleased to offer a variety of course offerings, including both performance and non-performance classes. Students at Harriton participate in a number of ensembles including Chorus, Concert Band, Concert Choir, Jazz Band, and Orchestra. In addition to the large ensembles, students also have the opportunity to perform in smaller chamber groups including Brass Ensemble, Chorale, String Quartet, and Woodwind Quintet. Our ensembles have received excellent and superior ratings at adjudicated festivals. In addition, our students are regularly chosen for participation in PMEA District, Region, and All-State Festivals."
         }
-        
-        
+            
+            
             
         else if segmentControl.selectedSegmentIndex == 2
         {
